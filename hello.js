792bf97439c69528cb29b2954399e96758072131
unknown <bondmandev@gmail.com>
2024-05-01 15:28:51 +0200

    Mon site

---
+++ hello.js
@@ -0,0 +1,61 @@
+var sel= document.querySelectorAll('.sel');
+var det= document.querySelector('.det');
+
+
+
+sel[0].addEventListener('mouseover',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.5)";
+	
+})
+sel[0].addEventListener('mouseout',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.3)";
+	
+})
+sel[1].addEventListener('mouseover',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.5)";
+	
+})
+sel[1].addEventListener('mouseout',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.3)";
+	
+})
+sel[2].addEventListener('mouseover',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.5)";
+	
+})
+sel[2].addEventListener('mouseout',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.3)";
+	
+})
+sel[3].addEventListener('mouseover',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.5)";
+	
+})
+sel[3].addEventListener('mouseout',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.3)";
+	
+})
+sel[4].addEventListener('mouseover',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.5)";
+	
+})
+sel[4].addEventListener('mouseout',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.3)";
+	
+})
+sel[5].addEventListener('mouseover',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.5)";
+	
+})
+sel[5].addEventListener('mouseout',function(){
+	this.style.backgroundColor="rgba(20,55,100,0.3)";
+	
+})
+
+
+
+
+
+
+
+
