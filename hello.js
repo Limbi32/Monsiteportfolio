bc55769fffa4ab96d3e8d093d9f26850f45fac04
Limbi32 <168622925+Limbi32@users.noreply.github.com>
2024-05-24 17:00:09 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,16 +1,23 @@
-var sel= document.querySelectorAll('.sel');
-var det= document.querySelector('.det');
+
+
 var cv=document.querySelector('.cv');
 var porto=document.querySelector('.porto');
-
+var menu=document.querySelector('.mn');
+var listmn=document.querySelector('.menu nav ul');
 
 cv.addEventListener('click',function(){
-	 
 	location.assign('cv.html');
 });
 porto.addEventListener('click',function(){
 	location.assign('portofolio.html');
 });
+menu.addEventListener('click',function () {
+	listmn.style.display='flex';
+	listmn.style.flexDirection='column';
+
+});
+
+
 
 
 
