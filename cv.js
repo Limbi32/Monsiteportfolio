dda3b3370d4ea4a41a3ba26540a65448803f367e
Limbi32 <168622925+Limbi32@users.noreply.github.com>
2024-05-30 09:57:46 +0200

    Update cv.js

--- cv.js
+++ cv.js
@@ -1,9 +1,11 @@
 var sel= document.querySelectorAll(' li');
 var menu=document.querySelector('.mn');
 var listmn=document.querySelector('.menu nav ul');
+var nav =document.querySelector('nav');
 menu.addEventListener('click',function () {
    listmn.style.display='flex';
 	listmn.style.flexDirection='column';
+   nav.style.flexDirection='column-reverse';
 });
 
 
