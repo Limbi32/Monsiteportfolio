f74ee40cca6b86eb825ca6fa990fc7ef280bb8f5
Limbi32 <168622925+Limbi32@users.noreply.github.com>
2024-05-30 09:59:31 +0200

    Update porto.js

--- porto.js
+++ porto.js
@@ -1,6 +1,8 @@
 var menu=document.querySelector('.mn');
 var listmn=document.querySelector('.menu nav ul');
+var nav =document.querySelector('nav');
 menu.addEventListener('click',function () {
 	listmn.style.display='flex';
 	listmn.style.flexDirection='column';
+	nav.style.flexDirection='column-reverse';
 });
